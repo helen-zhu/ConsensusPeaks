9f5936c9401de4e13cfd0b67fe3899bba51c1148
Stefan Eng <stefaneng@mednet.ucla.edu>
2021-06-11 09:21:09 -0700

    Attempt at finding the maximal meaningful intervals and gaps

--- inst/histogram.segmentationa.maxgap.R
+++ inst/histogram.segmentationa.maxgap.R
@@ -71,18 +71,20 @@ set.seed(5)
 n1 = rnorm(100, mean = 5, sd = 2)
 n2 = rnorm(50, mean = 10, sd = 3)
 n3 = rnorm(200, mean = 30, sd = 1)
-n.combined = c(n1, n2, n3)
+n4 = rnorm(150, mean = 50, sd = 1)
+n.combined = c(n1, n2, n3, n4)
 x = obs.to.int.hist(n.combined)
 plot.segments(x)
 
 # Running FTC
-s <- ftc.helen(x, s = sort(unlist(local.minmax(x))))
-s2 <- ftc(x)
+s = ftc.helen(x, s = sort(unlist(local.minmax(x))))
+s2 = ftc(x)
 plot.segments(x, unlist(s))
 plot.segments(x, s2)
 
 # Trying Maximal Meaningful
-todo = expand.grid(c(1:35), c(1:35))
+span = seq_along(x)
+todo = expand.grid(span, span)
 todo = todo[todo$Var2 > todo$Var1,]
 mint = do.call(rbind, lapply(1:nrow(todo), function(i) {
   meaningful.interval(
@@ -105,13 +107,14 @@ mgap = do.call(rbind, lapply(1:nrow(todo), function(i) {
   )
 }))
 df = cbind(todo, mint, mgap)
-df = df[order(df$Var1, df$Var2),]
+# df = df[order(df$Var2, df$Var1),]
+df = df[order(df$entropy), ]
 df$mint = as.numeric(df$mint)
 df$mgap = as.numeric(df$mgap)
 df$scaled_entropy = (df$entropy - min(df$entropy)) / (max(df$entropy) - min(df$entropy))
 
 # mint.cond = df$mint > 0
-# plotting.mint <- matrix(NA, nrow = sum(mint.cond), ncol = 35)
+# plotting.mint = matrix(NA, nrow = sum(mint.cond), ncol = 35)
 # plotting.mint
 
 # Create A Heatmap Looking For Meaningful Segments
@@ -130,73 +133,73 @@ bp = create.barplot(
   ylab.cex = 1
 )
 
-rgb2col = function(rgbmat){
-  ProcessColumn = function(col){
-    rgb(rgbmat[1, col],
-        rgbmat[2, col],
-        rgbmat[3, col],
-        maxColorValue = 255)
-  }
-  sapply(1:ncol(rgbmat), ProcessColumn)
-}
-
 seg.int.data = df[df$mint > 0, ]
 seg.int.data$index = 1:nrow(seg.int.data)
-seg.int = create.segplot(
-  formula = index ~ Var1 + Var2,
-  data = seg.int.data,
-  segments.col = rgb2col(t(colorRamp(c("blue", "orange"))(seg.int.data$scaled_entropy))),
-  xlab.label = '',
-  ylab.label = '',
-  yaxis.lab = NULL,
-  xaxis.lab = NULL
-)
 
 seg.gap.data = df[df$mgap > 0, ]
 seg.gap.data$index = 1:nrow(seg.gap.data)
-seg.gap = create.segplot(
-  formula = index ~ Var1 + Var2,
-  data = seg.gap.data,
-  segments.col = rgb2col(t(colorRamp(c("blue", "orange"))(seg.gap.data$scaled_entropy))),
-  xlab.label = '',
-  ylab.label = '',
-  yaxis.lab = NULL
-)
-# hm.int = create.heatmap(
-#   plotting.mint,
-#   clustering.method = "none",
-#   ylab.label = "Meaningful Intervals",
-#   xaxis.cex = 0.8,
-#   yaxis.cex = 0.8,
-#   ylab.cex = 1,
-#   xaxis.lab = colnames(plotting.mint),
-#   yaxis.lab = rownames(plotting.mint),
-#   xaxis.rot = F,
-#   colour.scheme = c("white", "black"),
-#   same.as.matrix = T,
-#   print.colour.key = F
-# )
 
-# hm.gap = create.heatmap(
-#   plotting.mgap,
-#   clustering.method = "none",
-#   ylab.label = "Meaningful Gaps",
-#   xaxis.cex = 0.8,
-#   yaxis.cex = 0.8,
-#   ylab.cex = 1,
-#   xaxis.lab = colnames(plotting.mgap),
-#   yaxis.lab = rownames(plotting.mgap),
-#   xaxis.rot = F,
-#   colour.scheme = c("white", "black"),
-#   same.as.matrix = T,
-#   print.colour.key = F
-# )
+# Quick and dirty implementation of finding maximal meaningful intervals (or gaps)
+maximal.meaningful = function(x) {
+  curr.df = x
+  max.intervals = data.frame()
+  while(nrow(curr.df) > 0) {
+    max.entropy.index = which.max(curr.df$entropy)
+    max.entropy = curr.df[max.entropy.index, ]
+    max.entropy.seq = seq(max.entropy$Var1, max.entropy$Var2)
+    max.intervals = rbind(max.intervals, max.entropy)
+
+    # Find all of the segments that overlap.
+    # These will all be less than the maximum
+    overlap.max = mapply(function(from, to) {
+      s = seq(from, to)
+      length(intersect(max.entropy.seq, s)) > 0
+    }, from = curr.df$Var1, to = curr.df$Var2)
+
+    curr.df = curr.df[!overlap.max, ]
+  }
+  # Preserve the old index
+  max.intervals$index = rownames(max.intervals)
+  rownames(max.intervals) <- NULL
+  max.intervals
+}
+
+max.intervals = maximal.meaningful(seg.int.data)
+max.gaps = maximal.meaningful(seg.gap.data)
+
+# Construct data for heatmap
+maximal.data <- rep("none", length(x))
+for(i in rownames(max.intervals)) {
+  r = max.intervals[i,]
+  int.seq = seq(r$Var1, r$Var2)
+  maximal.data[int.seq] <- "interval"
+}
+
+for(i in rownames(max.gaps)) {
+  r = max.gaps[i,]
+  gap.seq = seq(r$Var1, r$Var2)
+  maximal.data[gap.seq] <- "gap"
+}
+maximal.data <- factor(maximal.data, levels = c("none", "gap", "interval"))
+
+colour.scheme = c('white', 'blue', 'orange')
+maximal.heatmap <- create.heatmap(
+  x = t(as.matrix(as.numeric(maximal.data))),
+  clustering.method = 'none',
+  scale.data = FALSE,
+  colour.scheme = colour.scheme,
+  total.colours = 1 + length(colour.scheme),
+  grid.col = TRUE,
+  print.colour.key = FALSE,
+  force.grid.col = TRUE,
+  axes.lwd = 0
+)
 
 filename = "plots/Meaningful.Segments.pdf"
 #pdf(filename, height = 12, width = 8)
 create.multipanelplot(
-  plot.objects = list(bp, seg.int, seg.gap),
-  plot.objects.heights = c(1, 4, 4),
+  plot.objects = list(bp, maximal.heatmap),
+  plot.objects.heights = c(1, 0.05),
   y.spacing = -1,
   ylab.label = "Segments",
   xlab.label = "Index",
