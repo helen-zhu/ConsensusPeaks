fb0d452956b676bbfb9637f71dcc6c075af7a053
helen-zhu <helen.m.zhu@gmail.com>
2021-06-15 15:32:30 -0400

    minor bug, sometimes there are no gaps sigh

--- R/remove.max.gaps.R
+++ R/remove.max.gaps.R
@@ -1,6 +1,8 @@
 
 remove.max.gaps = function(geneinfo, seg.gr, max.gaps){
-  mgap.gr = GenomicRanges::GRanges(seqnames = geneinfo$chr, IRanges::IRanges(start = max.gaps$Var1, end = max.gaps$Var2), strand = geneinfo$strand)
-  seg.gr = GenomicRanges::setdiff(seg.gr, mgap.gr)
+  if(nrow(max.gaps) > 0){
+    mgap.gr = GenomicRanges::GRanges(seqnames = geneinfo$chr, IRanges::IRanges(start = max.gaps$Var1, end = max.gaps$Var2), strand = geneinfo$strand)
+    seg.gr = GenomicRanges::setdiff(seg.gr, mgap.gr)
+  }
   seg.gr
 }
