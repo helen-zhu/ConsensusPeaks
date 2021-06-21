ab967eab16bbe8469e8062c13e629b3c126269a3
helen-zhu <helen.m.zhu@gmail.com>
2021-06-21 16:54:51 -0400

    fixing weird bug where gap removal triggers merging of segments

--- R/remove.max.gaps.R
+++ R/remove.max.gaps.R
@@ -1,8 +1,17 @@
 
 remove.max.gaps = function(geneinfo, seg.gr, max.gaps){
   if(nrow(max.gaps) > 0){
+
+    # Creating a GenomicRanges Object with replicates of max gap
     mgap.gr = GenomicRanges::GRanges(seqnames = geneinfo$chr, IRanges::IRanges(start = max.gaps$Var1, end = max.gaps$Var2), strand = geneinfo$strand)
+    mgap.id = sort(rep(1:length(seg.gr), length(mgap.gr)))
+    mgap.gr = rep(mgap.gr, length(seg.gr))
+    mgap.gr = split(mgap.gr, mgap.id)
+
+    # Identifying the difference in set size
+    seg.gr = split(seg.gr, 1:length(seg.gr))
     seg.gr = GenomicRanges::setdiff(seg.gr, mgap.gr)
+    seg.gr = unlist(seg.gr)
   }
   seg.gr
 }
